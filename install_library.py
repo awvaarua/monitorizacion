43945defbd3bab0258a2df522581018efdbd8ac2
awvaarua <ganstersb2@gmail.com>
2016-10-28 19:05:33 +0200

    Added Template to check library and import if not exists

---
+++ install_library.py
@@ -0,0 +1,17 @@
+password = "fura4468AB\n"
+
+def restart():
+    import sys, os
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+try:
+    import requests
+    print "Paquete importado"
+except ImportError:
+    from subprocess import Popen, PIPE
+    process = Popen(['sudo', '-H', 'pip', 'install', 'requests'])
+    process.communicate(password)
+    process.wait()
+    print "Instalado requests"
+    restart()
